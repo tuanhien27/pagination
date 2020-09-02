6d348bd14bc63088e12b49c6422f9905fa4e817e
Tuanhien27 <vodanhtieutot27@gmail.com>
2020-09-02 17:47:01 +0700

    abc

--- pagin.js
+++ pagin.js
@@ -1,43 +1,68 @@
 // phân trang
 
-var arrTotal = [1,2,3,4,5,6,7];
+var arrTotal = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 
-var pagination = Math.ceil((arrTotal.length)/5); 
-console.log(pagination); //  pagination = 2
-
-var arrShow = [];
-
-for (let i = 1; i <= pagination; i++) {
-    arrShow.push(i);
-}
-console.log(arrShow); //[1,2]
-
-var addButton = arrShow.map(function(value){
-    return `
-    <button onclick="showPage(${value})">${value}</button> 
-    `
-  // khác bọt là chỗ chèn thẳng ${value} vào làm tham số dạng template string
-})
-//console.log(addButton); // ra được 2 button trong mảng
-
-var listPage = document.getElementById('list-page');
-listPage.innerHTML = addButton.join(''); // dc 3 button, do thành 1 string
-//ok
-
-
-
-function showPage(val) {
-    console.log(`đây là page thứ: ${val}`); //1,2
-    for (let indeX = 1; indeX < arrTotal.length; indeX++) {
-        if (((indeX < (val * 5)) && (indeX > (val - 1)*5)) && (indeX % 5 == 1) ) {
-            console.log(`indeX là:${indeX}`);
-            var arrFinish = arrTotal.slice(indeX-1,indeX+4);
-            console.log('kết quả là: ' + arrFinish);
-        }
+function beforePagin() {
+    console.log('mảng nhập vào:' + arrTotal);
+    var pagination = Math.ceil((arrTotal.length)/5); 
+    //console.log(pagination); //  
+    
+    var arrShow = [];
+    for (let i = 1; i <= pagination; i++) {
+        arrShow.push(i);
     }
+    console.log('mảng các pagination: ' + arrShow); 
+    
+    var addButton = arrShow.map(function(value){
+        return `
+        <button class="button-total" onclick="showPage(${value})">${value}</button> 
+        `
+      // khác bọt là chỗ chèn thẳng ${value} vào làm tham số dạng template string
+    })
+    //console.log(addButton); // ra được 2 button trong mảng
+    var listPage = document.getElementById('list-page');
+    listPage.innerHTML = addButton.join(''); // dc 3 button, do thành 1 string
+
 }
 
+beforePagin(); // tạo button tương ứng với số pagination
 
+
+function showPage(val) { //show các page khi click vào button tương ứng
+    console.log(`đây là pagination thứ: ${val}`); //1,2,3
+   
+    for (let indeX = 1; indeX <= arrTotal.length; indeX++) {
+        if (((indeX < (val * 5)) && (indeX > (val - 1)*5)) && (indeX % 5 == 1) ) {
+            console.log(`indeX là:${indeX}`); //1
+            var arrFinish = arrTotal.slice(indeX-1,indeX+4); 
+            console.log('các trang là: ' + arrFinish); // tìm dc các phần tử trong mảng khi log 1,2,3,4,5 //6
+            
+            arrFinish.forEach(function(v) {
+                console.log('giá trị là '+v);
+                let detail = document.querySelector('#page-detail'); // đây là bố, vật bám
+                let btn = document.createElement('button'); // đây là con, gắn element cho con, tương tác vs thèn con
+                btn.setAttribute("class", "button-child"); // class là button-child
+                
+                console.log(btn);
+                btn.innerHTML = `
+                <a>${v}</a>
+                `
+                detail.appendChild(btn); // con bám vào bố => ok
+            })
+            
+
+        }
+
+    let btnHidden = document.querySelector('.hidden');
+    btnHidden.style.display = "block";
+    }
+
+    pageOf(); 
+}
+
+function pageOf() {
+    console.log('chào cả nhà');
+}
 
 
 //---------------------------------
